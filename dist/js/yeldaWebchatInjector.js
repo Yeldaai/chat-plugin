ab4c8bb30fb39355b0552a4bdf36fd30d3e022f3
Vishnu <balavishnu.r@krds.fr>
2019-01-07 15:43:32 +0100

    renaming openOnLoad to shouldBeOpened

--- dist/js/yeldaWebchatInjector.js
+++ dist/js/yeldaWebchatInjector.js
@@ -192,7 +192,7 @@ var YeldaChat = function () {
       url = this.updateQueryStringParameter(url, 'assistantId', data.assistantId);
       url = this.updateQueryStringParameter(url, 'assistantSlug', data.assistantSlug);
       url = this.updateQueryStringParameter(url, 'locale', data.locale);
-      url = this.updateQueryStringParameter(url, 'openOnLoad', data.openOnLoad);
+      url = this.updateQueryStringParameter(url, 'shouldBeOpened', data.shouldBeOpened);
 
       if (data.isAdmin) {
         url = this.updateQueryStringParameter(url, 'isAdmin', data.isAdmin);
@@ -365,7 +365,7 @@ var YeldaChat = function () {
       data.chatUrl = data.assistantUrl + data.chatPath;
       data.locale = data.locale || 'fr_FR';
       data.isAdmin = data.isAdmin ? true : false;
-      data.openOnLoad = data.openOnLoad ? true : false;
+      data.shouldBeOpened = data.shouldBeOpened ? true : false;
 
       return data;
     }
