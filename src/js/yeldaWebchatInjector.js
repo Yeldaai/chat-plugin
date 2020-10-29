6656ab4d368bb5a6e5b57da553b146848d5d1aae
Vishnu <balavishnu.r@krds.fr>
2020-10-29 10:35:59 +0100

    const api url  used

--- src/js/yeldaWebchatInjector.js
+++ src/js/yeldaWebchatInjector.js
@@ -124,7 +124,7 @@ class YeldaChat {
   */
   getAssistantSettings(data, callback) {
     const xhr = new XMLHttpRequest();
-    const url= `${data.assistantUrl}/assistants/${data.assistantId}/chatBubble/${data.locale}`
+    const url= `${data.assistantUrl}assistants/${data.assistantId}/chatBubble/${data.locale}`
     xhr.open("GET", url);
     xhr.send();
 
